820c42d1c74fc85ec31e747bcc2c8ce0128ead28
EchhOpp <ngthluan285@gmail.com>
2024-09-09 17:35:12 +0700

    Complete: Lab03-#InputLogin

--- React-Navite/Lab-03/Lab03_InputLogin01/app/(tabs)/index.tsx
+++ React-Navite/Lab-03/Lab03_InputLogin01/app/(tabs)/index.tsx
@@ -1,21 +1,53 @@
-import { View, Text, ScrollView } from 'react-native'
+import { View, Text, ScrollView, TouchableOpacity, TextInput} from 'react-native'
 import React from 'react'
 import styles from '@/components/style'
 import Ionicons from '@expo/vector-icons/Ionicons';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 export default function index() {
-  return (
-    <ScrollView style={styles.main}>
-      <View style={styles.container}>
-        <View style={styles.header}>
-        <Ionicons name="git-commit" style={styles.icon} />
+return (
+  <View style={styles.container}>
+    <View style={styles.header}>
+      <Text  style={styles.title}>LOGIN</Text>
+    </View>
+    <View style={styles.body}>
+      <View style={styles.login}>
+        <View style={styles.input}>
+          <View style={styles.username}>
+            <TextInput style={styles.inputusername} placeholder="Email" placeholderTextColor={'#000000'}/>
+          </View>
+          <View style={styles.password}>
+            <TextInput style={styles.inputpassword } placeholder="Password" placeholderTextColor={'#000000'}/>
+            <View style={styles.contaiIcon}>
+              <FontAwesome style={styles.icon} name="eye"/>
+            </View>
+          </View>
         </View>
-        <View style={styles.body}>
-        <FontAwesome name="home" size={20} color="#000" />
+        <View style={styles.button}>
+          <TouchableOpacity style={styles.btnLogin}>
+            <Text style={styles.textLogin}>
+              LOGIN
+            </Text>
+          </TouchableOpacity>
         </View>
-        <View style={styles.footer}></View>
       </View>
-    </ScrollView>
-  )
+      <View style={styles.fotget}>
+        <Text style={styles.text}>When you agree to term and conditions</Text>
+        <Text style={[styles.text, styles.textblue]}>For got your password?</Text>
+        <Text style={styles.text}>Or login with</Text>
+      </View>
+    </View>
+    <View style={styles.footer}>
+      <View style={styles.iconfooter}>
+        <Ionicons style={styles.icon_f} name="logo-facebook"/>
+      </View>
+      <View style={styles.iconfooter}>
+        <Ionicons style={styles.icon_f} name="logo-twitter"/>
+      </View>
+      <View style={styles.iconfooter}>
+        <Ionicons style={styles.icon_f} name="logo-google"/>
+      </View>
+    </View>
+</View>
+)
 }
--- React-Navite/Lab-03/Lab03_InputLogin01/components/style.tsx
+++ React-Navite/Lab-03/Lab03_InputLogin01/components/style.tsx
@@ -1,37 +1,142 @@
 import { StyleSheet } from 'react-native'
 
 const styles = StyleSheet.create({
-    main: {
-        flex: 1,
-        backgroundColor: '#fff',
-        width: '100%',
-        height: '100%',
-    },
+    // Container
     container: {
+        flex: 1,
+        marginHorizontal: 30,
+        justifyContent: 'center',
+        alignItems: 'center',
+
+    },
+    // Header
+    header: {
+        flex: 1,
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    title: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: 'black',
+    },
+
+    // Body
+    body: {
+        flex: 2,
+        width: '100%',
+    },
+    login: {
+        flex: 3,
+        width: '100%',
+    },
+    input: {
+        flex: 2,
+        width: '100%',
+        alignContent: 'space-between',
+    },
+    username: {
+        flex: 1,
+        alignItems: 'center',
+    },
+    inputusername: {
+        width: '100%',
+        height: '60%',
+        backgroundColor: '#dddddd',
+        borderRadius: 4,
+        paddingHorizontal: 20,
+        fontSize: 20,
+    },
+
+    password: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+
+    inputpassword: {
+        width: '75%',
+        height: '60%',
+        marginVertical: 10,
+        backgroundColor: '#dddddd',
+        borderTopLeftRadius: 4,
+        borderBottomLeftRadius: 4,
+        paddingHorizontal: 20,
+        fontSize: 20,
+    },
+    contaiIcon: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '25%',
+        height: '60.2%',
+        backgroundColor: '#dddddd',
+        borderTopRightRadius: 4,
+        borderBottomRightRadius: 4,
+        
+    },
+
+    icon: {
+        fontSize: 30,
+        color: 'black',
+    },
+
+    button: {
+        flex: 1,
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    btnLogin: {
+        width: '100%',
+        height: '60%',
+        backgroundColor: '#f53d2d',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 4,
+    },
+    textLogin: {
+        fontSize: 20,
+        color: 'white',
+        fontWeight: 'bold',
+    },
+
+    fotget: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
     },
-    header: {
-        flex: 1,
-        backgroundColor: 'red',
-        width: '100%',
+    text: {
+        fontSize: 16,
+        color: 'black',
     },
-    body: {
-        flex: 2,
-        backgroundColor: 'blue',
-        width: '100%',
+    textblue: {
+        color: 'blue',
     },
+
+    // Footer
     footer: {
         flex: 1,
-        backgroundColor: 'green',
         width: '100%',
+        flexDirection: 'row',
     },
-    icon: {
-        fontSize: 90,
-        color: 'green',
-        height: 100,
+
+    iconfooter: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '50%',
+        backgroundColor: '#1593c6',
+        marginHorizontal: 5,
+        borderRadius: 4,
     },
+    icon_f: {
+        fontSize: 40,
+        color: 'white',
+    },
+
 })
 
 export default styles
