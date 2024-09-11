daf96e0ac9fe886db1e35b7355786dd08dd8b038
EchhOpp <ngthluan285@gmail.com>
2024-09-11 23:18:02 +0700

    Add: ReactNavite-Lab04-#DetailProduct

---
+++ React-Navite/Lab-04/Lab04_DetailProduct/app/index.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function _layout() {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+
+  }
+})
