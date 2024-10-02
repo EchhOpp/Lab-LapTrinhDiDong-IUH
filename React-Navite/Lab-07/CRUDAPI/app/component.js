fb2729ba10e4df3a3ad94abcccc531d424539d6e
EchhOpp <ngthluan285@gmail.com>
2024-10-03 00:32:12 +0700

    Fix: Lab07 - Addhandler data by fecth API

--- React-Navite/Lab-07/CRUDAPI/app/component.js
+++ React-Navite/Lab-07/CRUDAPI/app/component.js
@@ -1,10 +1,22 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity} from 'react-native'
 import React from 'react'
 
-const component = () => {
+const component = ({obj}) => {
   return (
-    <View>
-      <Text>component</Text>
+    <View style={{flexDirection: 'row', justifyContent: 'space-between', margin: 10, alignItems:'center'}}>
+      <Text>{obj.name}</Text>
+      <View style={{flexDirection: 'row', alignItems: 'center'}}>
+        <TouchableOpacity style={{padding: 8, backgroundColor: 'grey', margin: 10}}>
+          <Text style={{color: 'white', fontWeight: 'bold'}}>
+            Edit
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={{padding: 8, backgroundColor: 'grey', margin: 10}}>
+          <Text style={{color: 'white', fontWeight: 'bold'}}>
+            Delete
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
--- React-Navite/Lab-07/CRUDAPI/app/index.js
+++ React-Navite/Lab-07/CRUDAPI/app/index.js
@@ -1,5 +1,6 @@
 import { View, Text ,SafeAreaView, TouchableOpacity, TextInput, FlatList} from 'react-native'
 import React, {useEffect, useState} from 'react'
+import Component from './component';
 
 const index = () => {
   const [data, setData] = useState([]);
@@ -14,26 +15,48 @@ const index = () => {
     }, []
   );
 
+  const handleAdd = () => {
+    const newData = { 
+      name: 'New User', 
+      avatar: 'https://example.com/new-avatar.jpg', 
+      createdAt: new Date().toISOString() 
+    };
+  
+    fetch('https://6458c5718badff578efa564b.mockapi.io/api/todos', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newData)
+    })
+    .then((response) => response.json())
+    .then((json) => {
+      setData((currentData) => [...currentData, json]);
+      setInput('');
+    })
+    .catch((error) => console.error(error));
+  };
+
+
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{flex: 1}}> 
       <View>
         <TextInput placeholder='Nhập tên'style={{borderWidth:1, borderColor:'black', padding : 10, margin: 10}}/>
-        <TouchableOpacity style={{backgroundColor:'blue', padding: 10, margin: 10}}>
+        <TouchableOpacity style={{backgroundColor:'blue', padding: 10, margin: 10}} onPress={handleAdd}>
           <Text style={{color:'white', textAlign:'center', fontWeight:'bold'}}>
             Add
           </Text>
         </TouchableOpacity>
       </View>
-      <View>
+      <View style={{flex: 1}}>
         <FlatList
           data={data}
           keyExtractor={
             ({id}, index) => id
           }
           renderItem={
-            ({item}) => <Text>
-                           {item.name}
-                        </Text>
+            ({item}) => <Component obj={item}></Component>
           }
         />
       </View>
